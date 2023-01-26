02613a311cbd6f02645dc2dc22474d7a0869610c
Cioscos <claudiocicca1998@gmail.com>
2023-01-26 09:46:56 +0100

    Enhanced start.bat

--- start.bat
+++ start.bat
@@ -1,12 +1,19 @@
 @echo off
 
 rem Creating a Python virtual environment
+rem if it doesn't exist
+
+if exist ".\venv\" (
+	goto :run
+)
+
 python -m venv venv
-
 call .\venv\Scripts\activate
-
 pip install -r requirements.txt
 
+:run
+
+call .\venv\Scripts\activate
 python main.py -s dataset
 
 pause
